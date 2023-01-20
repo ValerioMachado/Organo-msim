2428b5efd5842a2648f4e0a56e92c0ca9f9d9ecd
Valerio <valeriom40@gmail.com>
2023-01-20 08:33:01 -0300

    Update Banner.js

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -5,7 +5,7 @@ export const Banner = () => {
     return (
         <header className="banner">
             <img src="/imagens/logo.png" alt="O banner principal da página do Organo"/>
-            <h1>Times, cargos e Coladores</h1>
+            <h1>Times, cargos e Colaboradores</h1>
         </header>
     )
 }
