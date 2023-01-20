34b1a5b7b4c483225b6c2a18b5589cdb6ba023fa
Valerio <valeriom40@gmail.com>
2023-01-20 03:58:13 -0300

    Projetinho Alura Final

---
+++ README.md
@@ -0,0 +1,9 @@
+Para visualizar esse projeto é necessário:
+
+instalar as dependencias 
+
+    npm install dependencies
+
+e dar um start
+
+    npm start
---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/logo.png" alt="O banner principal da página do Organo"/>
+            <h1>Times, cargos e Coladores</h1>
+        </header>
+    )
+}
